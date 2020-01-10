74c62732b4a61c958e35542c6c2804dd6776468f
thinhpm <phamminhthinh9x@gmail.com>
2020-01-10 18:18:40 +0700

    remove content

--- watchlakorn-and-content/3/test.py
+++
@@ -1,6 +0,0 @@
-from moviepy.editor import VideoFileClip, concatenate_videoclips
-clip1 = VideoFileClip("temp.mp4")
-clip2 = VideoFileClip("input2.mp4")
-final_clip = concatenate_videoclips([clip1,clip2])
-
-final_clip.write_videofile("my_concatenation.mp4")
